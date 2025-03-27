e46236ac6118db577adcc2c48eef7f1bcd6640ba
Andrew Motko <andrew.motko@gmail.com>
2025-03-27 20:41:48 +0200

    init: project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+        repellendus aliquid tempora ducimus culpa ea rem, dolore, consectetur
+        sequi suscipit cumque earum ipsum nobis? At reprehenderit quam iste
+        deleniti repellendus.
+      </p>
+    </>
+  );
+}
+
+export default App;
