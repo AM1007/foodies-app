8f63b9960e0f7f4957f2ee8432df5e218849f1e6
Andrew Motko <andrew.motko@gmail.com>
2025-04-27 18:44:26 +0300

    add: CORS settings & error handling functions

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,33 @@
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import 'dotenv/config';
 
+import notFoundHandler from './middlewares/notFoundHandler.js';
+import errorHandler from './middlewares/errorHandler.js';
+
 const app = express();
+
+// Логування запитів
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short';
+app.use(morgan(formatsLogger));
+
+// Налаштування CORS
+app.use(cors());
+
+// Парсери для обробки JSON та URL-encoded даних
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+// Обробка неіснуючих маршрутів
+app.use(notFoundHandler);
+
+// Обробка помилок
+app.use(errorHandler);
 
 const { PORT = 3000 } = process.env;
 const port = Number(PORT);
---
+++ backend/middlewares/errorHandler.js
@@ -0,0 +1,14 @@
+const errorHandler = (err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+
+  // Логуємо помилку в консоль
+  console.error(
+    `${status} - ${message} - ${req.originalUrl} - ${req.method} - ${req.ip}`,
+  );
+
+  res.status(status).json({
+    message,
+  });
+};
+
+export default errorHandler;
