a627f02dccb05ec3d930f3603d45435e55782ea2
Anna Komrakova <komrakova.anna@gmail.com>
2025-04-29 23:00:50 +0100

    logo update

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,11 +1,16 @@
+import { Link } from 'react-router-dom';
 import styles from './Logo.module.css';
 import logo from '/public/logo.svg';
+// import icons from '../../icons/sprite.svg';
 
 const Logo = () => {
   return (
-    <>
+    <Link to="/" className="flex items-center">
       <img src={logo} alt="Site logo" className={styles.logo} />
-    </>
+      {/* <svg width="136" height="16">
+          <use href={`${icons}#logo`} />
+        </svg> */}
+    </Link>
   );
 };
 
