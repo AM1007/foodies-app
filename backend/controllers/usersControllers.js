91cf75ce7ca8cd1f32bd684fdbfb41b6f293349d
kryvosheyin <alex.kryvosheyin@gmail.com>
2025-04-30 15:58:38 +0100

    apped option to update avatar and store it in the cloudinary

--- backend/controllers/usersControllers.js
+++ backend/controllers/usersControllers.js
@@ -1,1 +1,29 @@
-//this_is_a_temporary_comment
+import usersServices from '../services/usersServices.js';
+import ctrlWrapper from '../decorators/ctrlWrapper.js';
+import HttpError from '../helpers/HttpError.js';
+import { HTTP_STATUS } from '../constants/auth.js';
+
+const getUserInfo = async (req, res) => {
+  res.status(HTTP_STATUS.OK).json({ user: req.user });
+};
+
+const updateAvatar = async (req, res) => {
+  if (!req.file) {
+    return res
+      .status(HTTP_STATUS.BAD_REQUEST)
+      .json({ message: 'Please attach the file' });
+  }
+  const { email } = req.user;
+  const avatarUrl = req.file?.path;
+
+  if (!avatarUrl) {
+    throw HttpError(HTTP_STATUS.BAD_REQUEST, 'Image upload failed');
+  }
+  await usersServices.updateUserAvatar(email, avatarUrl);
+  res.json({ avatar: avatarUrl, message: 'Avatar was successfully updated' });
+};
+
+export default {
+  updateAvatar: ctrlWrapper(updateAvatar),
+  getUserInfo: ctrlWrapper(getUserInfo),
+};
--- backend/routes/usersRouter.js
+++ backend/routes/usersRouter.js
@@ -1,1 +1,17 @@
-//this_is_a_temporary_comment
+import express from 'express';
+import usersControllers from '../controllers/usersControllers.js';
+import upload from '../middlewares/upload.js';
+import authenticate from '../middlewares/authenticate.js';
+
+const userRouter = express.Router();
+
+userRouter.get('/current', authenticate, usersControllers.getUserInfo);
+
+userRouter.patch(
+  '/avatar',
+  authenticate,
+  upload.single('avatar'),
+  usersControllers.updateAvatar,
+);
+
+export default userRouter;
