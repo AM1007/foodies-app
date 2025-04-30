88ef285a364273de920d1e60a1ecb2ed0e700209
YNazymko12 <yulychka12@gmail.com>
2025-04-30 18:20:32 +0200

    added navigation, light and dark theme

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
-import Logo from '../Logo/Logo';
+import { useLocation } from 'react-router-dom';
+import Navigation from '../Navigation/Navigation';
 import styles from './Header.module.css';
 
 const Header = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
+  const headerThemeClass = isHomePage ? styles.darkTheme : styles.lightTheme;
   return (
     <>
-      <header className={styles.headerContainer}>
+      <header className={`${styles.headerContainer} ${headerThemeClass}`}>
         <div className={styles.headerWrapper}>
-          <Logo />
+          <Navigation isLightTheme={!isHomePage} />
           <div className={styles.buttons}>
             <button className={`${styles.button} ${styles.signIn}`}>
               SIGN IN
