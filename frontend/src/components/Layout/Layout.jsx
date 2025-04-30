6b8b11762cf4f2bc54ab8fda9ab05538d5bc3e94
Anna Komrakova <komrakova.anna@gmail.com>
2025-04-30 19:33:14 +0100

    footer skeleton, sprite

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from 'react-router-dom';
-import Footer from '../Footer/Footer';
+import Footer from '../sections/Footer/Footer';
 
 const Layout = () => {
   return (
---
+++ frontend/src/components/sections/Footer/Footer.jsx
@@ -0,0 +1,18 @@
+import Logo from '../../Logo/Logo';
+import NetworkLinks from '../../NetworkLinks/NetworkLinks';
+import styles from './Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={`container ${styles.footer}`}>
+      <div className={styles.wrapper}>
+        <Logo />
+
+        <NetworkLinks />
+      </div>
+      <p className={styles.copyright}>© 2024, Foodies. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
