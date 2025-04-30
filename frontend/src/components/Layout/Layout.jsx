b737b1da341fb37cb27f3e7c175769dbcf03aa35
Anna Komrakova <komrakova.anna@gmail.com>
2025-04-30 20:24:28 +0100

    fixes

--- frontend/src/components/Layout/Layout.jsx
+++ frontend/src/components/Layout/Layout.jsx
@@ -1,7 +1,7 @@
 import { Outlet } from 'react-router-dom';
 
-import Footer from '../sections/Footer/Footer';
 import Header from '../Header/Header';
+import Footer from '../sections/Footer/Footer';
 
 const Layout = () => {
   return (
