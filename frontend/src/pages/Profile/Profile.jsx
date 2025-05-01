968082e1682602f277a0ebc93b624425a34b326a
yanashapka <yana.shapka.test@gmail.com>
2025-05-02 00:07:19 +0300

    updated library

--- frontend/src/pages/Profile/Profile.jsx
+++ frontend/src/pages/Profile/Profile.jsx
@@ -5,6 +5,7 @@ import MainTitle from '../../components/ui/MainTitle/MainTitle';
 import Subtitle from '../../components/ui/Subtitle/Subtitle';
 import Button from '../../components/Button/Button';
 import RecipePreview from '../../components/RecipePreview/RecipePreview';
+import UserInfo from '../../components/UserInfo/UserInfo';
 
 const Profile = () => {
   const { openModal } = useModal();
@@ -15,6 +16,7 @@ const Profile = () => {
       <MainTitle text="Profile" />
       <Subtitle text="Reveal your culinary art, share your favorite recipe and create gastronomic masterpieces with us." />
       <Button onClick={() => openModal('logout')}>Log Out</Button>
+      <UserInfo/>
       <RecipePreview />
     </div>
   );
