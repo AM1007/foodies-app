e1d86523af6ba728b1a0c8ed47b39895c06791a1
tetiana zorii <tanyusha.zoriy@gmail.com>
2025-05-02 04:23:19 -0400

    add component time controller

--- frontend/src/components/AddRecipeForm/AddRecipeForm.jsx
+++ frontend/src/components/AddRecipeForm/AddRecipeForm.jsx
@@ -1,8 +1,31 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './AddRecipeForm.module.css';
+// import axios from 'axios';
+
+import DeleteBtn from '../ui/DeleteBtn/DeleteBtn';
+// import SelectCategory from '../SelectCategory/SelectCategory';
+import TimeController from '../ui/TimeController/TimeController';
 
 const AddRecipeForm = () => {
-  const [text, setText] = React.useState('');
+  // const [categories, setCategories] = useState([]);
+  // const [selectedCategory, setSelectedCategory] = useState('');
+  const [text, setText] = useState('');
+  const [time, setTime] = useState(10);
+
+  // useEffect(() => {
+  //   const fetchCategories = async () => {
+  //     try {
+  //       const response = await axios.get(
+  //         'https://foodies-app-pke3.onrender.com/api/categories',
+  //       );
+  //       setCategories(response.data);
+  //     } catch (error) {
+  //       console.error('Failed to fetch categories:', error);
+  //     }
+  //   };
+
+  //   fetchCategories();
+  // }, []);
 
   return (
     <>
@@ -20,66 +43,81 @@ const AddRecipeForm = () => {
 
       {/* Назва рецепту */}
       <div className={styles.formGroup}>
-        <label className={`${styles.nameLabel} ${styles.label}`}>THE NAME OF THE RECIPE</label>
+        <label className={`${styles.nameLabel} ${styles.label}`}>
+          The name of the recipe
+        </label>
         <div className={styles.inputWrapper}>
           <textarea
             className={styles.textarea}
             placeholder="Enter a description of the dish"
             maxLength={200}
             value={text}
-            onChange={(e) => setText(e.target.value)}
+            onChange={e => setText(e.target.value)}
           />
           <span className={styles.charCount}>{text.length}/200</span>
         </div>
       </div>
 
-      {/* Категорія */}
-      <div className={styles.formGroup}>
-        <label className={styles.label}>CATEGORY</label>
-        <div className={styles.selectWrapper}>
+      {/* Категорія, Час приготування, Інгредієнти, Кількість */}
+      <div className={styles.formWrapper}>
+        {/* Категорія */}
+        
+        <div>
+          <label className={styles.label}>Category</label>
+          {/* <SelectCategory
+            selectedCategory={selectedCategory}
+            onCategoryChange={setSelectedCategory}
+            categories={categories}
+          /> */}
+        </div>
+       
+
+        {/* Час приготування */}
+        <div>
+          <label className={styles.label}>Cooking time</label>
+          <TimeController value={time} onChange={setTime} />
+        </div>
+
+        {/* Інгредієнти */}
+        <div>
+          <label className={styles.label}>Ingredients</label>
           <select className={styles.select}>
-            <option value="" disabled selected>Select a category</option>
+            <option value="" >
+              Add the ingredient
+            </option>
           </select>
         </div>
-      </div>
 
-      {/* Час приготування */}
-      <div className={styles.formGroup}>
-        <label className={styles.label}>COOKING TIME</label>
-        <div className={styles.timeController}>
-          <button className={styles.timeButton}>−</button>
-          <span className={styles.timeValue}>10 min</span>
-          <button className={styles.timeButton}>+</button>
+        {/* Кількість */}
+        <div>
+          <input
+            type="text"
+            className={styles.input}
+            placeholder="Enter quantity"
+          />
         </div>
       </div>
 
-      {/* Інгредієнти */}
-      <div className={styles.formGroup}>
-        <label className={styles.label}>INGREDIENTS</label>
-        <div className={styles.ingredientInputs}>
-          <div className={styles.selectWrapper}>
-            <select className={styles.select}>
-              <option value="" disabled selected>Add the ingredient</option>
-            </select>
-          </div>
-          <input type="text" className={styles.input} placeholder="Enter quantity" />
-        </div>
-        <button className={styles.addIngredientButton}>
-          ADD INGREDIENT <span className={styles.plusIcon}>+</span>
-        </button>
-      </div>
+      {/* Кнопка додавання */}
+      <button className={styles.addIngredientButton}>
+        ADD INGREDIENT <span className={styles.plusIcon}>+</span>
+      </button>
 
       {/* Рецепт */}
       <div className={styles.formGroup}>
         <label className={styles.label}>RECIPE PREPARATION</label>
         <div className={styles.inputWrapper}>
-          <textarea className={styles.textarea} placeholder="Enter recipe"></textarea>
+          <textarea
+            className={styles.textarea}
+            placeholder="Enter recipe"
+          ></textarea>
           <span className={styles.charCount}>0/200</span>
         </div>
       </div>
 
       {/* Кнопки */}
       <div className={styles.actionsGroup}>
+        <DeleteBtn />
         <button className={styles.resetButton}>
           <svg width="48" height="48" fill="none"></svg>
         </button>
