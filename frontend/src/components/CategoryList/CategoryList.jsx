065f09d430059f22695b2b29688e18ad58b0023c
AlinaRyabova <115992255+AlinaRyabova@users.noreply.github.com>
2025-05-04 15:59:40 +0300

    update categoriesList

--- frontend/src/components/CategoryList/CategoryList.jsx
+++ frontend/src/components/CategoryList/CategoryList.jsx
@@ -1,21 +1,5 @@
 import styles from './CategoryList.module.css';
-
-const categories = [
-  { name: 'Beef', image: '/image/categories/Beef_1x-min.png' },
-  { name: 'Breakfast', image: '/image/categories/Breakfast_1x-min.png' },
-  { name: 'Desserts', image: '/image/categories/Desserts_1x-min.png' },
-  { name: 'Lamb', image: '/image/categories/Lamb_1x-min.png' },
-  { name: 'Goat', image: '/image/categories/Goat_1x-min.png' },
-  {
-    name: 'Miscellaneous',
-    image: '/image/categories/Miscellaneous_1x-min.png',
-  },
-  { name: 'Pasta', image: '/image/categories/Pasta_1x-min.png' },
-  { name: 'Pork', image: '/image/categories/Pork_1x-min.png' },
-  { name: 'Seafood', image: '/image/categories/Seafood_1x-min.png' },
-  { name: 'Side', image: '/image/categories/Side_1x-min.png' },
-  { name: 'Starter', image: '/image/categories/Starter_1x-min.png' },
-];
+import categories from '../../data/categories.js';
 
 export default function CategoryList({ onCategoryClick }) {
   return (
---
+++ frontend/src/data/categories.js
@@ -0,0 +1,16 @@
+const categories = [
+  { name: 'Beef', image: '/image/categories/Beef_1x-min.png' },
+  { name: 'Breakfast', image: '/image/categories/Breakfast_1x-min.png' },
+  { name: 'Desserts', image: '/image/categories/Desserts_1x-min.png' },
+  { name: 'Lamb', image: '/image/categories/Lamb_1x-min.png' },
+  { name: 'Goat', image: '/image/categories/Goat_1x-min.png' },
+  {
+    name: 'Miscellaneous',
+    image: '/image/categories/Miscellaneous_1x-min.png',
+  },
+  { name: 'Pasta', image: '/image/categories/Pasta_1x-min.png' },
+  { name: 'Pork', image: '/image/categories/Pork_1x-min.png' },
+  { name: 'Seafood', image: '/image/categories/Seafood_1x-min.png' },
+  { name: 'Side', image: '/image/categories/Side_1x-min.png' },
+  { name: 'Starter', image: '/image/categories/Starter_1x-min.png' },
+];
