1401213640339b9ab2c9bad85a8b3786304184d7
Anna Komrakova <komrakova.anna@gmail.com>
2025-05-05 21:17:53 +0100

    adding logout to userBar

--- frontend/src/components/UserBar/UserBar.jsx
+++ frontend/src/components/UserBar/UserBar.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import clsx from 'clsx';
 import { useSelector } from 'react-redux';
+import { useModal } from '../../hooks/useModal';
 import Button from '../Button/Button';
 import icons from '../../icons/sprite.svg';
 import styles from './UserBar.module.css';
@@ -8,6 +9,7 @@ import styles from './UserBar.module.css';
 const UserBar = () => {
   const user = useSelector(state => state.user.current) || {};
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const { openModal } = useModal();
   return (
     <div className={styles.userBar}>
       <button
@@ -30,7 +32,10 @@ const UserBar = () => {
       {isDropdownOpen && (
         <div className={styles.dropdown}>
           <Button className={styles.dropdownItem}>PROFILE</Button>
-          <Button className={styles.dropdownItem}>
+          <Button
+            className={styles.dropdownItem}
+            onClick={() => openModal('logout')}
+          >
             LOG OUT{' '}
             <svg className={styles.icon} width="18" height="18">
               <use href={`${icons}#arrow-up-right`} />
