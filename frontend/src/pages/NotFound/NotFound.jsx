b99081fc8af493c79a192a4fbc12e43e6167b908
Anna Komrakova <komrakova.anna@gmail.com>
2025-05-06 21:14:03 +0100

    not found page

--- frontend/src/pages/NotFound/NotFound.jsx
+++ frontend/src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,14 @@
+import css from './NotFound.module.css';
+import { Link } from 'react-router-dom';
+import imageNotFound from '../../icons/imageNotFound.svg';
+import Button from '../../components/Button/Button';
+
 const NotFound = () => {
   return (
-    <>
-      <p>NotFound</p>
-    </>
+    <div>
+      <img className={css.imageNotFound} src={imageNotFound} alt="Not found" />
+      <Link to={`/`}>{/* <Button>Home Page</Button> */}</Link>
+    </div>
   );
 };
 
