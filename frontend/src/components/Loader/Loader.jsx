705f86653b5340bf939e09a922aa9f4c30de224f
ViktorSvertoka <victor.svertoka@gmail.com>
2025-05-10 00:46:14 +0300

    feat(files): add loader

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -1,26 +1,25 @@
-import { CircleLoader } from 'react-spinners';
 import styles from './Loader.module.css';
 
-const override = {
-  display: 'block',
-  margin: '0 auto',
-  borderColor: 'red',
-};
-
 const Loader = () => {
-  const loading = true;
-  const color = '#36d7b7';
-
   return (
-    <div className={styles.loaderContainer}>
-      <CircleLoader
-        color={color}
-        loading={loading}
-        cssOverride={override}
-        size={150}
-        aria-label="Loading Spinner"
-        data-testid="loader"
-      />
+    <div className={styles.wrapper}>
+      {/* <h1 className={styles.title}>Loading...</h1> */}
+      <div className={styles.cooking}>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.bubble}></div>
+        <div className={styles.area}>
+          <div className={styles.sides}>
+            <div className={styles.pan}></div>
+            <div className={styles.handle}></div>
+          </div>
+          <div className={styles.pancake}>
+            <div className={styles.pastry}></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
