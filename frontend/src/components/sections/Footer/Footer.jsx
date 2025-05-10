65374b0d57f1baed2cc9c229864634f02e9ee0bb
ViktorSvertoka <victor.svertoka@gmail.com>
2025-05-11 02:32:58 +0300

    feat(files): add button to modal

--- frontend/src/components/sections/Footer/Footer.jsx
+++ frontend/src/components/sections/Footer/Footer.jsx
@@ -1,3 +1,4 @@
+import CircularText from '../../CircularText/CircularText';
 import Logo from '../../Logo/Logo';
 import NetworkLinks from '../../NetworkLinks/NetworkLinks';
 import styles from './Footer.module.css';
@@ -7,6 +8,12 @@ const Footer = () => {
     <footer className={`container ${styles.footer}`}>
       <div className={styles.wrapper}>
         <Logo />
+        <CircularText
+          text="SPACE*RACCOONS*TEAM*"
+          onHover="speedUp"
+          spinDuration={20}
+          className="custom-class"
+        />
 
         <NetworkLinks />
       </div>
