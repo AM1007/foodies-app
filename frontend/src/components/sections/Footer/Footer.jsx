71bfdc5e75d5c1485f1bea24b55f80a529a36421
ViktorSvertoka <victor.svertoka@gmail.com>
2025-05-11 05:03:16 +0300

    feat(files): add team model

--- frontend/src/components/sections/Footer/Footer.jsx
+++ frontend/src/components/sections/Footer/Footer.jsx
@@ -3,18 +3,19 @@ import Logo from '../../Logo/Logo';
 import NetworkLinks from '../../NetworkLinks/NetworkLinks';
 import styles from './Footer.module.css';
 
-const Footer = () => {
+const Footer = ({ openModal }) => {
   return (
     <footer className={`container ${styles.footer}`}>
       <div className={styles.wrapper}>
         <Logo />
-        <CircularText
-          text="SPACE*RACCOONS*TEAM*"
-          onHover="speedUp"
-          spinDuration={20}
-          className="custom-class"
-        />
-
+        <div onClick={() => openModal('team')}>
+          <CircularText
+            text="SPACE*RACCOONS*TEAM*"
+            onHover="speedUp"
+            spinDuration={20}
+            className="custom-class"
+          />
+        </div>
         <NetworkLinks />
       </div>
       <p className={styles.copyright}>
